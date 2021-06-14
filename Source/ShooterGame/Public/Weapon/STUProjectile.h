d1bcd036d3b7692f6e333ffbb74588bc08718434
Fudso <you@example.com>
2021-06-14 21:59:21 +0700

    Added new weapon: launcher

---
+++ Source/ShooterGame/Public/Weapon/STUProjectile.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class SHOOTERGAME_API ASTUProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ASTUProjectile();
+
+	void SetShotDirection(const FVector& Direction)
+	{
+		ShotDirection = Direction;
+	}
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Weapon")
+	USphereComponent* CollisionComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Weapon")
+	UProjectileMovementComponent* MovementComponent;
+
+	virtual void BeginPlay() override;
+
+private:
+	FVector ShotDirection;
+	
+};
