b5c93c4796023f123f415be06d48057595f67be1
nvk <nkn@mail.ru>
2021-06-15 17:28:48 +0700

    Character, Controller, Nav mesh

---
+++ Source/ShooterGame/Public/AI/STUAICharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/SGBaseCharacter.h"
+#include "STUAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API ASTUAICharacter : public ASGBaseCharacter
+{
+	GENERATED_BODY()
+
+	
+public:
+	ASTUAICharacter(const FObjectInitializer& ObjInit);
+};
