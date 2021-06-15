3e06f4051fe11d933fb4565029a33b59fd1afb7a
nvk <nkn@mail.ru>
2021-06-15 20:06:40 +0700

    Find aim service, decorator

--- Source/ShooterGame/Public/AI/STUAIController.h
+++ Source/ShooterGame/Public/AI/STUAIController.h
@@ -22,7 +22,13 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	USTUAIPerceptionComponent* STUAIPerceptionComponent;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	FName FocusOnKeyName = "EnemyActor";
+	
 
 	virtual void OnPossess(APawn* InPawn) override;
 	virtual void Tick(float DeltaSeconds) override;
+
+	private:
+	AActor* GetFocusOnActor() const;
 };
